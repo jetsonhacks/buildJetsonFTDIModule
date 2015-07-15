5df94876ee66ecbc70f9c36bce7f66a341643b13
jetsonhacks <jim@jetsonhacks.com>
2015-07-14 23:24:17 -0700

    Update version number

--- prepareModule.sh
+++ prepareModule.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Prepare to build some drivers for LT4 21.1 on the NVIDIA Jetson TK1
+# Prepare to build some drivers for LT4 21.4 on the NVIDIA Jetson TK1
 if [ $(id -u) != 0 ]; then
    echo "This script requires root permissions"
    echo "$ sudo "$0""
@@ -11,7 +11,7 @@ fi
 apt-add-repository universe
 apt-get update
 apt-get install libncurses5-dev -y
-# Get the kernel source for LT4 21.1
+# Get the kernel source for LT4 21.4
 cd /usr/src/
 wget http://developer.download.nvidia.com/embedded/L4T/r21_Release_v4.0/source/kernel_src.tbz2
 # Decompress
