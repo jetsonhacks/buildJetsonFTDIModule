f7bb370be4bc6d45722a0ce8dc058e282de1d0cc
jetsonhacks <jim@jetsonhacks.com>
2015-07-15 11:05:37 -0700

    Modify .config file straight away, add remove kernel sources script

--- prepareModule.sh
+++ prepareModule.sh
@@ -19,11 +19,22 @@ tar -xvf kernel_src.tbz2
 cd kernel
 # Get the kernel configuration file
 zcat /proc/config.gz > .config
-# And begin editing
-make menuconfig
+# Enable FTDI compilation
+sudo sed -i 's/# CONFIG_USB_SERIAL_FTDI_SIO is not set/CONFIG_USB_SERIAL_FTDI_SIO=m/' .config
+# Make sure that the local kernel version is set
+LOCALVERSION=$(uname -r)
+# vodoo incantation; This removes everything from the beginning to the last occurrence of "-"
+# of the local version string i.e. 3.10.40 is removed
+release="${LOCALVERSION##*-}"
+CONFIGVERSION="CONFIG_LOCALVERSION=\"-$release\""
+# Replace the empty local version with the local version of this kernel
+sudo sed -i 's/CONFIG_LOCALVERSION=""/'$CONFIGVERSION'/' .config
+# Prepare the module for compilation
 make prepare
 make modules_prepare
+# Compile the module
 make M=drivers/usb/serial/
+# After compilation, copy the compiled module to the system area
 cp drivers/usb/serial/ftdi_sio.ko /lib/modules/$(uname -r)/kernel
 depmod -a
 
---
+++ removeSources.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# Remove the kernel sources and build entrails
+sudo rm -r /usr/src/gtest
+sudo rm -r /usr/src/kernel
+sudo rm    /usr/src/kernel_src.tbz2
+
+
